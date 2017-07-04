4d2acb48cf30254e504e5daebf7112f0fa823694
averybunker <avery@bunkerlabs.us>
2017-07-04 13:44:33 -0400

    Initial Code

---
+++ Process_Restart/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Process_Restart
+{
+    class Program
+    {
+        static Boolean amIrunning = false;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ListProcesses();
+			if (amIrunning == false)
+			{
+                Console.WriteLine(RunCommand());
+			}
+        }
+
+		static void ListProcesses()
+		{
+            
+			Process[] processCollection = Process.GetProcesses();
+			foreach (Process p in processCollection)
+			{
+				Console.WriteLine(p.ProcessName);
+                if(p.ProcessName == "iTerm2")
+                {
+                    amIrunning = true;
+                }
+			}
+            Console.WriteLine(amIrunning.ToString());
+
+		}
+
+		public static bool RunCommand()
+		{
+			try
+			{
+				var procStartInfo = new ProcessStartInfo("/Applications/Calculator.app/Contents/MacOS/Calculator")
+                {
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+					CreateNoWindow = false
+				};
+
+				var proc = new Process { StartInfo = procStartInfo };
+				proc.Start();
+
+				// Get the output into a string
+				var output = proc.StandardOutput.ReadToEnd();
+
+                try
+                {
+                    return proc.ExitCode == decimal.Zero ? true : false;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    return false;
+                }
+
+			}
+			finally
+			{
+				// do something
+			}
+		}
+    }
+}
